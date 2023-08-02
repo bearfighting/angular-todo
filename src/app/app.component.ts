fe5b25da2e0098633f5f3d47f1f31eb225ac9490
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2023-08-02 11:39:36 -0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { TodoList } from './todo-list';
+import { TodoItem } from './todo-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList('Bob', [
+    new TodoItem('Go for run', true),
+    new TodoItem('Get flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter((item) => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter((item) => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem: string) {
+    this.list.addItem(newItem);
+  }
+
+  showComplete: boolean = false;
 }
